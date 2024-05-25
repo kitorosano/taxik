169fbe02438541403d8691dfbb23462ededdcce3
Kitorosano <rosanoesteban2@gmail.com>
2024-05-24 22:14:48 -0300

    feat: :sparkles: Gestionar Usuarios en el Dashboard de Administrador
    Listar, Modificar, Eliminar Usuarios. Control de autorizacion por usuario.

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -9,9 +9,8 @@ export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
 
-    const isLogged = user !== null;
-    const username = isLogged ? user.name : "Invitado";
-    const useremail = isLogged ? user.email : "";
+    const username = user ? user.name : "Invitado";
+    const useremail = user ? user.email : "";
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -33,6 +32,17 @@ export default function Authenticated({ user, header, children }) {
                                     Contactos
                                 </NavLink>
                             </div>
+
+                            {user && user.isAdmin && (
+                                <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                    <NavLink
+                                        href={route("users.index")}
+                                        active={route().current("users.index")}
+                                    >
+                                        Usuarios
+                                    </NavLink>
+                                </div>
+                            )}
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
@@ -62,7 +72,7 @@ export default function Authenticated({ user, header, children }) {
                                         </span>
                                     </Dropdown.Trigger>
 
-                                    {isLogged ? (
+                                    {user ? (
                                         <Dropdown.Content>
                                             <Dropdown.Link
                                                 href={route("profile.edit")}
@@ -155,6 +165,17 @@ export default function Authenticated({ user, header, children }) {
                         </ResponsiveNavLink>
                     </div>
 
+                    {user && user.isAdmin && (
+                        <div className="pt-2 pb-3 space-y-1">
+                            <ResponsiveNavLink
+                                href={route("users.index")}
+                                active={route().current("contacts.index")}
+                            >
+                                Usuarios
+                            </ResponsiveNavLink>
+                        </div>
+                    )}
+
                     <div className="pt-4 pb-1 border-t border-gray-200">
                         <div className="px-4">
                             <div className="font-medium text-base text-gray-800">
@@ -165,7 +186,7 @@ export default function Authenticated({ user, header, children }) {
                             </div>
                         </div>
 
-                        {isLogged ? (
+                        {user ? (
                             <div className="mt-3 space-y-1">
                                 <ResponsiveNavLink href={route("profile.edit")}>
                                     Perfil
