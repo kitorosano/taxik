57bac44fe809685576fb5ef4d0c5e79896345b9e
Kitorosano <rosanoesteban2@gmail.com>
2024-05-29 17:41:22 -0300

    feat: :art: Application Logo changed to Taxik

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -20,7 +20,7 @@ export default function Authenticated({ user, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo className="w-[56px] h-[56px]" />
                                 </Link>
                             </div>
 
