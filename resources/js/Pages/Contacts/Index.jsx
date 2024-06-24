7640dfe948c0886ff93d85eac88e4db3c820d56e
Kitorosano <rosanoesteban2@gmail.com>
2024-06-24 01:00:10 -0300

    refactor: :art: Se quita el padding inferior de las paginas principales
    fix: en la tabla de usuarios

--- resources/js/Pages/Contacts/Index.jsx
+++ resources/js/Pages/Contacts/Index.jsx
@@ -61,7 +61,7 @@ function Index({ auth, contacts, filters }) {
             }
         >
             <Head title="Contactos" />
-            <div className="py-10">
+            <div className="py-10 pb-0">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
--- resources/js/Pages/Orders/Index.jsx
+++ resources/js/Pages/Orders/Index.jsx
@@ -1,7 +1,7 @@
 import Pagination from "@/Components/Pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@moraki/inertia-react";
-import TravelOrdersClientTable from "./partials/TravelOrdersClientTable";
+import TravelOrdersClientTable from "./Partials/TravelOrdersClientTable";
 
 const columns = {
     status: "Estado",
@@ -26,7 +26,7 @@ function Index({ auth, orders }) {
             }
         >
             <Head title="Historial de reservas" />
-            <div className="py-10">
+            <div className="py-10 pb-0">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <TravelOrdersClientTable
