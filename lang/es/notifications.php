783dc177afee7fdcfe93474ef169572ba80b745e
Kitorosano <rosanoesteban2@gmail.com>
2024-07-02 07:21:48 -0300

    feat: :globe_with_meridians: Agregado evento de notificacion; Agregado localizacion para notificacion
    Agregado localizacion para enums de modelos

---
+++ lang/es/notifications.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+  /*
+    |--------------------------------------------------------------------------
+    | Notifications Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the notification library to build
+    | the simple notification messages. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+  'travel-order-update' => [
+    'client' => [
+      'title' => 'Reserva de viaje actualizada',
+      'message' => 'El estado de tu pedido #:id ha sido actualizado a :status.'
+    ],
+    'company' => [
+      'title' => 'Nueva solicitud de viaje!',
+      'message' => 'Tienes un nuevo pedido de viaje #:id de :client.'
+    ]
+  ]
+
+];
